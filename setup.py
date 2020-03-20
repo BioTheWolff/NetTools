0eb7f13113a2c6f5e5fe32a71516a4d4a7925049
BioTheWolff <contact.biowolf@gmx.fr>
2020-03-20 15:41:40 +0100

    Regression to v0.1.0 because of non-presence of wildcard-based definition of network range

--- setup.py
+++ setup.py
@@ -8,9 +8,9 @@ if sys.version_info[:2] < (3, 6):
     raise RuntimeError("Python version >= 3.6 required.")
 
 
-MAJOR = 1
-MINOR = 0
-MICRO = 1
+MAJOR = 0
+MINOR = 1
+MICRO = 0
 IS_RELEASED = True
 VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
 
